aaf252a1a31e78fb87e037d73e94f4b023113ef2
Darrell1017 <57148454+Darrell1017@users.noreply.github.com>
2019-12-05 11:42:31 -0500

    Update Playlist.cpp

--- group-project-2/Playlist.cpp
+++ group-project-2/Playlist.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <string>
+#include "Playlist.h"
 
+Playlist::Playlist(const std::string& name_love)
+    : name {name_love}
+{}
+
+
+void Playlist::addSong(Song&& song)
+{
+    length += song.songLength;
+    song_list.push_back(song);
+}
+
+void Playlist::deleteSong(Song && song)
+{
+    length 
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Playlist& value)
+{
+    os << "Playlist '" << value.name << "':\n";
+    for(const auto& s : value.song_list) {
+        os << "title: " << s.songTitle << "; artist: " << s.artistName
+           << "; genre: " << s.songGenre << "; duration " << s.songLength 
+           << '\n';
+    }
+    return os;
+}
